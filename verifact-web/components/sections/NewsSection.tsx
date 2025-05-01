f1f92ecb92a75b21c5f2fac858d08f0737921ddd
Pranjay Yadav <ypranjay@gmail.com>
2025-05-01 13:44:00 +0530

    ui changes in news section

--- verifact-web/components/sections/NewsSection.tsx
+++ verifact-web/components/sections/NewsSection.tsx
@@ -244,7 +244,7 @@ const NewsSection = ({ news }: NewsProps) => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true }}
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4 gap-8"
       >
         {isLoading ? (
           // Show skeleton cards while loading
